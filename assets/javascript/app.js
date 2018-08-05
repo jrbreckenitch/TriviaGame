af830ef3b3064c0d6910a16a81c43b71e16eaa7c
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-08-05 13:08:06 -0500

    working timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,64 @@
 // when timer runs out check buttons with booleans and tally for score
 
 $(document).ready(function() {
-    
-var newDiv = $("<div>");
+
+var correct = 0;
+
+var incorrect = 0;
+
+var unanswered = 0;
+
+var timeDiv = $("<p>'Time Remaining: '</p>");
 
 $(".triviaQuestions").hide();
+$("#scoreBox").hide();
+$("#timeLeft").hide();
 
-    $("#startButton").on("click", function() {
-        $("#startRow").empty();
-        $(".triviaQuestions").show();  
-    });
+
+$("#startButton").on("click", function() {
+    $("#startRow").empty();
+    $(".triviaQuestions").show();
+    $("#timeLeft").show();  
+
+var n = 10;
+setTimeout(countDown,1000);
+
+function countDown(){
+    n--;
+    if(n > 0){
+        $("#counter").html("<p>Seconds Remaining: </p>" + n )
+        setTimeout(countDown,1000);
+    }
+    else if (n === 0) {
+        $(".triviaQuestions").hide();
+        $("#timeLeft").hide(); 
+        $("#scoreBox").show();
+    }
+    console.log(n);
+}
+
+// var interval = setInterval(function()
+// {
+//     document.getElementById("cco").innerHTML = -- cc;
+
+//     if (cc == 0)
+//         clearInterval(interval);
+
+// }, 1000);
+
+    // setTimeout(secondCount, 1000);
+
+    // function secondCount() {
+
+    //     for (i = 120; i > 0; i--) {
+    //         $("#timeLeft").html("<p>Time Remaining: </p>" + i);
+    //     }
+
+    //     // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+    //     // console log 10 seconds left
+    //     $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
+    //     console.log("10 seconds left");
+    // }
+});
 
 });

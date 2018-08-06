cab2a41d1abc1be8272cbfc265926f0f5bb01266
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-08-05 20:22:07 -0500

    game updated with unique questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,32 +19,145 @@ var winDiv = $("<p>Correct Answer: " + correct  + "</p>");
 var lossDiv = $("<p>Wrong Answer: " + incorrect  + "</p>");
 
 
-// $('input[name=name_of_your_radiobutton]:checked').val();
-
-// var scoreCheck = function() {
-//     if ($('input[Vader]:checked', '#questions1').val() === true) {
-//         correct++;
-//         $("#correctAnswer").html(winDiv);
-//         // $("#wins").text("Wins:" + wins);
-//     }
-//     else if ($('input[Vader]:checked', '#questions1').val() === false) {
-//         incorrect++;
-//         $("#wrongAnswer").html(lossDiv);
-//         // $("#losses").text("Losses:" + incorrect);
-//     }
-// }
-
-var scoreCheck = function() {
-    if ($('input:radio[name=q1]')[1].checked === true) {
+var scoreCheck1 = function() {
+    if ($('input:radio[name=q1]')[3].checked === true) {
         correct++;
     }
     else if ($('input:radio[name=q1]')[0].checked === true) {
         incorrect++;
     }
+    else if ($('input:radio[name=q1]')[1].checked === true) {
+        incorrect++;
+    }
     else if ($('input:radio[name=q1]')[2].checked === true) {
         incorrect++;
     }
-    else if ($('input:radio[name=q1]')[3].checked === true) {
+    else {
+        unanswered++;
+    }
+}
+
+var scoreCheck2 = function() {
+    if ($('input:radio[name=q2]')[0].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q2]')[1].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q2]')[2].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q2]')[3].checked === true) {
+        incorrect++;
+    }
+    else {
+        unanswered++;
+    }
+}
+
+var scoreCheck3 = function() {
+    if ($('input:radio[name=q3]')[3].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q3]')[0].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q3]')[1].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q3]')[2].checked === true) {
+        incorrect++;
+    }
+    else {
+        unanswered++;
+    }
+}
+
+var scoreCheck4 = function() {
+    if ($('input:radio[name=q4]')[0].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q4]')[1].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q4]')[2].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q4]')[3].checked === true) {
+        incorrect++;
+    }
+    else {
+        unanswered++;
+    }
+    // $("#correctAnswer").html("<p>Correct Answers: " + correct + "</p>");
+    // $("#wrongAnswer").html("<p>Wrong Answers: " + incorrect + "</p>");
+    // $("#noAnswer").html("<p>Unanswered Questions: " + unanswered + "</p>");
+};
+var scoreCheck5 = function() {
+    if ($('input:radio[name=q5]')[1].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q5]')[0].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q5]')[2].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q5]')[3].checked === true) {
+        incorrect++;
+    }
+    else {
+        unanswered++;
+    }
+}
+
+var scoreCheck6 = function() {
+    if ($('input:radio[name=q6]')[3].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q6]')[0].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q6]')[1].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q6]')[2].checked === true) {
+        incorrect++;
+    }
+    else {
+        unanswered++;
+    }
+}
+
+var scoreCheck7 = function() {
+    if ($('input:radio[name=q7]')[0].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q7]')[1].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q7]')[2].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q7]')[3].checked === true) {
+        incorrect++;
+    }
+    else {
+        unanswered++;
+    }
+}
+
+var scoreCheck8 = function() {
+    if ($('input:radio[name=q8]')[2].checked === true) {
+        correct++;
+    }
+    else if ($('input:radio[name=q8]')[0].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q8]')[1].checked === true) {
+        incorrect++;
+    }
+    else if ($('input:radio[name=q8]')[3].checked === true) {
         incorrect++;
     }
     else {
@@ -56,6 +169,7 @@ var scoreCheck = function() {
 
 };
 
+
 $(".triviaQuestions").hide();
 $("#scoreBox").hide();
 $("#timeLeft").hide();
@@ -66,7 +180,7 @@ $("#startButton").on("click", function() {
     $(".triviaQuestions").show();
     $("#timeLeft").show();  
 
-var n = 6;
+var n = 31;
 setTimeout(countDown,1000);
 
 function countDown(){
@@ -76,7 +190,15 @@ function countDown(){
         setTimeout(countDown,1000);
     }
     else if (n === 0) {
-        scoreCheck();
+        scoreCheck1();
+        scoreCheck2();
+        scoreCheck3();
+        scoreCheck4();
+        scoreCheck5();
+        scoreCheck6();
+        scoreCheck7();
+        scoreCheck8();
+
         $(".triviaQuestions").hide();
         $("#timeLeft").hide(); 
         $("#scoreBox").show();
@@ -86,28 +208,8 @@ function countDown(){
     console.log(n);
 }
 
-// var interval = setInterval(function()
-// {
-//     document.getElementById("cco").innerHTML = -- cc;
 
-//     if (cc == 0)
-//         clearInterval(interval);
-
-// }, 1000);
-
-    // setTimeout(secondCount, 1000);
-
-    // function secondCount() {
-
-    //     for (i = 120; i > 0; i--) {
-    //         $("#timeLeft").html("<p>Time Remaining: </p>" + i);
-    //     }
-
-    //     // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-    //     // console log 10 seconds left
-    //     $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-    //     console.log("10 seconds left");
-    // }
 });
 
 });
+
